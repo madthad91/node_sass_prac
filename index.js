99259ce6ea3f552bdf1bce1872315c5e62b7b283
Thaddeus Madison Jr <thaddeus.madison@gmail.com>
2017-06-29 22:04:37 -0500

    working via npm scripts. still not working using nodejs node sass + buffered output

---
+++ index.js
@@ -0,0 +1,26 @@
+var sass = require('node-sass');
+
+var result = sass.renderSync({
+  file: 'index.scss',
+  includePaths: ['public'],
+  // data: 'body{background:blue; a{color:black;}}',
+  outputStyle: 'compressed',
+  outFile: 'output.css',
+  sourceMap: true, // or an absolute or relative (to outFile) path
+  importer: function(url, prev, done) {
+ 
+    console.log(url, prev, "madthad91");
+    return {file: result.path, contents: result.data};
+  },
+  success: function(result) {
+    // you will need to write result.css to file-system, we just return you a string
+    // same goes for result.map
+    console.log('hai me', result)
+  }
+});
+
+var exec = require('child_process').exec;
+
+exec('export me2="'+JSON.stringify(result)+ '"; $me2 > lezgo; echo $me2 ', function (error, stdout, stderr) {
+    console.log(stdout);
+});
